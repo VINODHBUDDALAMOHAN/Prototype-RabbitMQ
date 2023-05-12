3d2ea58fc615ae23a1f7659816583d3e3380e7b2
Buddalamohan, Vinodh <vinodh.buddalamohan@Weatherford.com>
2023-05-12 15:48:49 +0100

    changes

--- LocalMachineSetup/ConfigureRabbitMQ/ReadMe.md
+++ LocalMachineSetup/ConfigureRabbitMQ/ReadMe.md
@@ -1,6 +1,6 @@
 ﻿Setting up docker RabbitMQ docker on local machine
 1. run docker command
-docker run -d --name acq-rabbit -p 5672:5672 -p 15672:15672  -e RABBITMQ_MNESIA_BASE=/var/lib/rabbitmq/mnesia rabbitmq:3-management --restart always
+docker run -d --name acq-rabbit -p 5672:5672 -p 15672:15672 -p 1833:1833  -e RABBITMQ_MNESIA_BASE=/var/lib/rabbitmq/mnesia rabbitmq:3-management --restart always
 
 2.dotnet run ConfigureRabbitMQ.
 
--- LocalMachineSetup/TelemetrySource/Program.cs
+++ LocalMachineSetup/TelemetrySource/Program.cs
@@ -12,15 +12,22 @@ namespace IoTSensorApplication;
 
 class Program
 {
-    private static ConnectionFactory? _factory;
-    private static IConnection? _connection;
-    private static IModel? _model;
-
+    private static bool _useMqtt = true;
     //This code creates a connection and sends a message 10 times.
-    static void Main()
+    static async Task Main()
     {
-        //Create a connection
-        CreateConnection();
+        Action<string> sendMessage;
+        if (!_useMqtt)
+        {
+            var model = CreateConnection();
+            sendMessage = (msg) => { SendMessage(model, msg); };
+        }
+        else
+        {
+            var mqttClient = await CreateMqttConnection();
+            sendMessage = (msg) => { SendMqttMessage(mqttClient, msg).Wait(); };
+        }
+
         //Initialize a counter
         int i = 0;
         //Loop 10 times
@@ -29,32 +36,76 @@ class Program
             //Increment the counter
             i++;
             //Send a message with the counter and a new GUID
-            SendMessage($$"""{'Id': 'device-client-01', 'msg': '{{i}}-{{Guid.NewGuid()}}'}""");
+            sendMessage($$"""{'Id': 'device-client-01', 'msg': '{{i}}-{{Guid.NewGuid()}}'}""");
         }
     }
     //This code creates a connection to a RabbitMQ server.
-    private static void CreateConnection()
+    private static IModel CreateConnection()
     {
         //Create a new ConnectionFactory object with the necessary parameters
-        _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest", Port = 5672 };
+        var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest", Port = 5672 };
 
         //Create a connection using the ConnectionFactory
-        _connection = _factory.CreateConnection();
+        var connection = factory.CreateConnection();
 
         //Create a model using the connection
-        _model = _connection.CreateModel();
+        var model = connection.CreateModel();
+
+        return model;
     }
 
+
     //This code sends a message to a RabbitMQ exchange
-    private static void SendMessage(string message)
+    private static void SendMessage(IModel model, string message)
     {
         //Create a basic properties object
-        var basicProperties = _model.CreateBasicProperties();
+        var basicProperties = model.CreateBasicProperties();
         //Set the message to be persistent
         basicProperties.Persistent = true;
         //Publish the message to the exchange
-        _model.BasicPublish(Configurations.AcquitionExchange_Exchange.ExchangeName, "", basicProperties, Encoding.UTF8.GetBytes(message));
+        model.BasicPublish(Configurations.AcquitionExchange_Exchange.ExchangeName, "", basicProperties, Encoding.UTF8.GetBytes(message));
         //Print out the message that was sent
+        Console.WriteLine($"Sent {message}");
+    }
+
+    private static async Task<IMqttClient> CreateMqttConnection()
+    {
+        // Create a connection factory
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            Port = 1883,
+            UserName = "guest",
+            Password = "guest",
+            ClientProvidedName = "device-client-01"
+        };
+
+        // Create an MQTT client
+        var mqttFactory = new MqttFactory();
+        var mqttClient = mqttFactory.CreateMqttClient();
+
+        // Create an options object for the MQTT client
+        var mqttOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer(factory.HostName, factory.Port)
+            .WithCredentials(factory.UserName, factory.Password)
+            .WithClientId(factory.ClientProvidedName)
+            .WithCleanSession()
+            .Build();
+
+        await mqttClient.ConnectAsync(mqttOptions);
+
+        return mqttClient;
+    }
+    private static async Task SendMqttMessage(IMqttClient mqttClient, string message)
+    {
+        var messagePayload = new MqttApplicationMessageBuilder()
+            .WithTopic(Configurations.AcquitionExchange_Exchange.ExchangeName)
+            .WithPayload(message)
+            .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+            .Build();
+
+        await mqttClient.PublishAsync(messagePayload);
+
         Console.WriteLine($"Sent {message}");
     }
 
